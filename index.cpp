df03e480017bf51954c4de1e674b96c736651c4c
facusteckler86 <facundomsteckler@gmail.com>
2025-04-20 21:02:26 -0300

    detallo main

--- index.cpp
+++ index.cpp
@@ -6,12 +6,13 @@
 #include <limits> /* libreria para limites de tipos*/
 #include <cmath> /* libreria de funciones matematicas*/
 #include <ctime> /* libreria para funciones de tiempo*/ 
+using namespace std; /*espacio de nombres estandar, para no tener que escribir siempre std::*/
 
 int main() /*main de la app*/
 {
   std::string name;
-  std::cout << "What is your name? ";
+  std::cout << "What is your name? "; /* cout es lo que se ve en terminal */
   getline (std::cin, name);
-  std::cout << "Hello, " << name << "!\n";
+  std::cout << "Hello, " << name << "!\n"; /* <<endl es salto de linea*/
   return 0; /*retorno de la funcion main*/
 }
